ac292d14adce90c0dd2754d49d8178dbbf7ce34d
yenfharm <yenfhael.arm@gmail.com>
2024-01-12 14:26:34 -0400

    changes

--- w01-task.js
+++ w01-task.js
@@ -16,7 +16,7 @@ console.log(`The total is ${theTotal}`);
 // 'total' function declaration
 function total(...theNumbers) {
 	let sum = 0;
-	for (let aNumber in theNumbers) {
+	for (let aNumber of theNumbers) {
 		sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
 	}
 	return sum
