528ddd85a8e68c23ba959ebb8005c497af24f2fc
yenfharm <yenfhael.arm@gmail.com>
2024-01-21 00:09:06 -0400

    changes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -13,10 +13,10 @@ let profilePicture = "images/profile.jpg";
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById('name');
-nameElement.textContent = "Yenfhael Armas";
+// nameElement.textContent = "Yenfhael Armas";
 
 const foodElement = document.getElementById('food');
-foodElement.textContent = "Potatoes"
+// foodElement.textContent = "Potatoes"
 
 const yearElement = document.querySelector('#year');
 yearElement.textContent = "2024";
@@ -27,17 +27,22 @@ imageElement.src = '/images/profile.jpg';
 
 /* Step 4 - Adding Content */
 
-
-
-
-
-
+nameElement.innerHTML = '<strong>Yenfhael Armas</strong>' //I can typing my name in this way or like is on 16 line of the code
+yearElement.textContent = currentYear
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 
 /* Step 5 - Array */
 
+let favoriteFoods = ["pizza", "burger", "potatoes"];
 
+let anotherFavoriteFood = "sushi";
+favoriteFoods.push(anotherFavoriteFood);
+foodElement.innerHTML += "<br>" + favoriteFoods;
 
+favoriteFoods.shift();
+foodElement.innerHTML += "<br>" + favoriteFoods;
 
-
-
+favoriteFoods.pop();
+foodElement.innerHTML += "<br>" + favoriteFoods;
