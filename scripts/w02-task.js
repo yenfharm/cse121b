83893a65e39882fe1fd34dd4a70671fe912c666b
yenfharm <yenfhael.arm@gmail.com>
2024-01-20 23:08:34 -0400

    changes

---
+++ scripts/w02-task.js
@@ -0,0 +1,43 @@
+/* W02-Task - Profile Home Page */
+
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName = "Yenfhael Armas";
+let currentYear = 2024;
+let profilePicture = "images/profile.jpg";
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+nameElement.textContent = "Yenfhael Armas";
+
+const foodElement = document.getElementById('food');
+foodElement.textContent = "Potatoes"
+
+const yearElement = document.querySelector('#year');
+yearElement.textContent = "2024";
+
+const imageElement = document.getElementsByTagName("img")[0];
+imageElement.src = '/images/profile.jpg';
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
