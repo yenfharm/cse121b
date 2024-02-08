965c38d90aac3bc4a58f496121adddcf80b0e65e
yenfharm <yenfhael.arm@gmail.com>
2024-02-08 10:57:44 -0400

    new

---
+++ scripts/w05-task.js
@@ -0,0 +1,20 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+
+
+/* async displayTemples Function */
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* filterTemples Function */
+
+
+getTemples();
+
+/* Event Listener */
